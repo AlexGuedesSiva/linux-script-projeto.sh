56092ce0ca8410ddfa05dbb5358785f01f5b1c5d
AlexGuedesSiva <alex__guedes@hotmail.com>
2022-08-12 23:17:43 +0000

    ATT

--- criar_user.sh
+++ criar_user.sh
@@ -5,6 +5,7 @@ echo"Criando usuario em serie...."
 useradd convidade1 -c "convidado especial1" -s /bin/bash -m -p $(openssl passwd 123)
 useradd convidade2 -c "convidado especial2" -s /bin/bash -m -p $(openssl passwd 123)
 useradd convidade3 -c "convidado especial3" -s /bin/bash -m -p $(openssl passwd 123)
+useradd leon -c "Leon Kennedy" -s /bin/bash -m 
 
 echo"Fim ......"
 
